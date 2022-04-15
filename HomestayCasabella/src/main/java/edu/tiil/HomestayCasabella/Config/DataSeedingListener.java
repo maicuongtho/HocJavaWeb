603f54aaf42396c59c846e5010dfe7a1ed10ac81
maicuongtho <thomc@ntu.edu.vn>
2022-04-15 15:34:21 +0700

    bo commmnet

--- HomestayCasabella/src/main/java/edu/tiil/HomestayCasabella/Config/DataSeedingListener.java
+++ HomestayCasabella/src/main/java/edu/tiil/HomestayCasabella/Config/DataSeedingListener.java
@@ -28,36 +28,36 @@ public class DataSeedingListener implements ApplicationListener<ContextRefreshed
     @Override
     public void onApplicationEvent(ContextRefreshedEvent arg0) {
         // Roles
-//        if (roleRepository.findByName("ROLE_ADMIN") == null) {
-//            roleRepository.save(new Role("ROLE_ADMIN"));
-//        }
-//
-//        if (roleRepository.findByName("ROLE_MEMBER") == null) {
-//            roleRepository.save(new Role("ROLE_MEMBER"));
-//        }
-//
-//        // Admin account
-//        if (userRepository.findByEmail("admin@gmail.com") == null) {
-//            User admin = new User();
-//            admin.setEmail("admin@gmail.com");
-//            admin.setPassword(passwordEncoder.encode("123456"));
-//            HashSet<Role> roles = new HashSet<>();
-//            roles.add(roleRepository.findByName("ROLE_ADMIN"));
-//            roles.add(roleRepository.findByName("ROLE_MEMBER"));
-//            admin.setRoles(roles);
-//            userRepository.save(admin);
-//        }
-//
-//        // Member account
-//        if (userRepository.findByEmail("member@gmail.com") == null) {
-//            User user = new User();
-//            user.setEmail("member@gmail.com");
-//            user.setPassword(passwordEncoder.encode("123456"));
-//            HashSet<Role> roles = new HashSet<>();
-//            roles.add(roleRepository.findByName("ROLE_MEMBER"));
-//            user.setRoles(roles);
-//            userRepository.save(user);
-//        }
+        if (roleRepository.findByName("ROLE_ADMIN") == null) {
+            roleRepository.save(new Role("ROLE_ADMIN"));
+        }
+
+        if (roleRepository.findByName("ROLE_MEMBER") == null) {
+            roleRepository.save(new Role("ROLE_MEMBER"));
+        }
+
+        // Admin account
+        if (userRepository.findByEmail("admin@gmail.com") == null) {
+            User admin = new User();
+            admin.setEmail("admin@gmail.com");
+            admin.setPassword(passwordEncoder.encode("123456"));
+            HashSet<Role> roles = new HashSet<>();
+            roles.add(roleRepository.findByName("ROLE_ADMIN"));
+            roles.add(roleRepository.findByName("ROLE_MEMBER"));
+            admin.setRoles(roles);
+            userRepository.save(admin);
+        }
+
+        // Member account
+        if (userRepository.findByEmail("member@gmail.com") == null) {
+            User user = new User();
+            user.setEmail("member@gmail.com");
+            user.setPassword(passwordEncoder.encode("123456"));
+            HashSet<Role> roles = new HashSet<>();
+            roles.add(roleRepository.findByName("ROLE_MEMBER"));
+            user.setRoles(roles);
+            userRepository.save(user);
+        }
     }
 
 }
